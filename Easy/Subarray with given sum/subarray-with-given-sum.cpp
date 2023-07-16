79b68a665cf3be39d6ee0cee02c3afe524142027
Sourav Kumar <77422786+sk-108@users.noreply.github.com>
2023-07-16 20:02:10 +0530

    Updated Solution - GfG to GitHub

--- Easy/Subarray with given sum/subarray-with-given-sum.cpp
+++ Easy/Subarray with given sum/subarray-with-given-sum.cpp
@@ -10,13 +10,13 @@ class Solution
     //Function to find a continuous sub-array which adds up to a given number.
     vector<int> subarraySum(vector<int>arr, int n, long long s)
     {
-        if(s == 0)return {-1};
+        
         int i=0;
         int j=0;
         int sum = 0;
         while(j<=i && i<=n)
         {
-            
+            if(s == 0)return {-1};
              if(sum > s)
             {
                 sum -= arr[j];
