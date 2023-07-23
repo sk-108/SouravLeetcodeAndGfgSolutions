eda3d0549e55d56c4542548b696dc5e29c51398c
Sourav Kumar <77422786+sk-108@users.noreply.github.com>
2023-07-23 22:14:19 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Greater on right side/greater-on-right-side.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* Function to replace every element with the
+	next greatest element */
+	void nextGreatest(int arr[], int n) {
+	    // code here
+	    int gre = -1;
+	    for(int i = n-1;i>=0;i--)
+	    {
+	        if(arr[i] > gre)
+	        {
+	            int temp = arr[i];
+	            arr[i] = gre;
+	            gre = temp;
+	        }
+	        else{
+	            arr[i] = gre;
+	        }
+	    }
+	}
+
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, l, r;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.nextGreatest(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
